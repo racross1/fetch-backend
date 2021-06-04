b9c007d8889ca85d91fc528234f4c0555d41b0fe
racross1 <racross1@gmail.com>
2021-06-03 21:00:36 -0500

    logic to transaction model and controller

--- fetch-backend/app/controllers/transactions_controller.rb
+++ fetch-backend/app/controllers/transactions_controller.rb
@@ -2,9 +2,22 @@ class TransactionsController < ApplicationController
 
 # class methods to be added to create 
 def create
-    amount = 
+    #have amount check here and in transaction controller as well
+    #here so that transaction not processed and error message is shown
     
-    transaction = Transaction.create(user_id: )
+    #all of the below needs to be tested and connected to frontend. just coded here in order to shape how model methods done
+    amount = transaction_params[:init_amount]
+    user = User.find(transaction_params[:user_id])
+    user_bal = user['pts_balance']
+
+    if amount > user_bal
+        render json: { error: 'failed to create user' }
+    else  
+        transaction = Transaction.create(user_id: transaction_params[:user_id], transaction_params[:payer_id], init_amount: amount, active_amount: amount)
+        transaction_summary = transaction.process_new_transaction
+        render json: transaction_summary
+    end
+   
     #check user balance
     #if requested amount exceeds user balance, give error message
 
@@ -21,3 +34,8 @@ end
 end
 
 
+private
+
+def transaction_params 
+    params.require(:transaction).permit(:user_id, :payer_id, :init_amout)
+end
--- fetch-backend/app/models/transaction.rb
+++ fetch-backend/app/models/transaction.rb
@@ -2,21 +2,19 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :payer
 
-  def process_new_transaction(amount, user, payer)
-    user_bal = user["pts_balance"]
-    payer_bal = payer["pts_balance"]
+  def self.get_active_transactions
+     active_transactions = Transaction.all.select{|t| t[active_amount] != 0}
+     #sort descending to cut down time complexity
+     sorted_active_transactions = active_transactions.sort_by{|t| t[created_at]}.reverse
+     return sorted_active_transactions
 
-    #add var for new abalance for each and update user and payer point
-    #bals to reflect this
-    #where to do check for no nonzero
-    #here i think -> if amount > user pts bal then errror message
+  end 
 
+  #transaction has already been validated as less than payer balance 
+  #and therefore created
 
-    if amount > 0
-      user.update(pts_balance: )
-
-
-    ##takes arg of transaction amount, user and payer
+  #scratch notes
+    ##takes arg of transaction
 
     ##if positive, update user and payer bals
 
@@ -29,16 +27,62 @@ class Transaction < ApplicationRecord
     ##update current user and payer bal (ensure neither goes below zero)
     
     ##method to get current user and payer bal
-    ##method to 
+    
+    #check if amount greater than user bal?
+   #if so, just for CLI application
+
+    #add var for new abalance for each and update user and payer point
+    #bals to reflect this
+    #where to do check for no nonzero
+    #here i think -> if amount > user pts bal then errror message
+
+  def process_new_transaction(transaction)
+    user = User.find(transaction[user_id])
+    payer = Payer.find(transaction[payer_id])
+    amount = transaction[init_amount]
+
+    user_bal = user[pts_balance]
+    payer_bal = payer[pts_balance]
+
+    #this check is done in transaction controller within create method
+    #repeated here in case of use in CLI / for adding transactions in seed file
+    if amount > user_bal 
+      puts "insufficient funds - add more points!"
+      return 
+    end 
+
+  
+    if amount >= 0
+      user.update(pts_balance: user_bal + amount)
+      payer.update(pts_balance: payer_bal + amount)
+      #tbd what gets returned here
+      #have separate method to check if it's a valid transaction (to be used in controller and here)? 
+      return 
+    else
+      #make transaction 0 so that it does not show up in get_active transaction list
+      #before this point will have already been validated that user has sufficient funds
+      transaction.update(active_amount: 0) 
+      active_transactions = Transaction.get_active_transactions
+      output = transaction.get_transaction_output(active_transactions, amount)
+    end 
 
 
   end 
   
-  def update_payer_bal
-  end 
+  def get_transaction_output(active_transactions, amount)
+    running_sum = amount
+    i = 0
+    while running_sum > 0
+      curr = active_transactions[i]
 
-  def update_user_bal
-  end 
+      #logic here for running total that finds cutoff point then allocates
+      #to payers accordingly
+
+      
+    end 
+
+  end
+
 
 
 end
